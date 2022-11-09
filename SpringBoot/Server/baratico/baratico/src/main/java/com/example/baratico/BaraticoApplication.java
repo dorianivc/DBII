857445d6870a1d3fd678e952ee0fc7ae9f75d7a3
NormanBetancourth <norman.betancourtt.mairena@est.una.ac.cr>
2022-11-08 23:11:39 -0600

    Inicializando

---
+++ SpringBoot/Server/baratico/baratico/src/main/java/com/example/baratico/BaraticoApplication.java
@@ -0,0 +1,13 @@
+package com.example.baratico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaraticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaraticoApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/Server/baratico/baratico/src/test/java/com/example/baratico/BaraticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baratico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaraticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
